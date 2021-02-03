de2733b337108846bfc516b290e4a70ada7ca640
Cmart94 <cristian.313@hotmail.com>
2021-02-03 23:31:46 +0000

    4-puts.c excercise

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Alwaus 0 (Success)
+ */
+init main(void)
+{
+	puts "\"Programming is like building a multilingual puzzle";
+	return (0);
+}
+
