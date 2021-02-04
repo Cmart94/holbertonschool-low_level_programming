5bf730cd3529d3574bb4709bbdbb8aa25069231c
Cmart94 <cristian.313@hotmail.com>
2021-02-04 19:22:46 +0000

    fix 03 y 04 excersice

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  *main - Entry point
  *
  *Return: Alwaus 0 (Success)
  */
-init main(void)
+int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  *main - Entry point
  *
  *Return: Alwaus 0 (Success)
  */
-init main(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
