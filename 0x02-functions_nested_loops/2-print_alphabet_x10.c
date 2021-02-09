0a407362bfbbf6f70c8214af984861a4c0d66aed
Cmart94 <cristian.313@hotmail.com>
2021-02-09 21:36:59 +0000

    2-print_alphabet_x10 excersice

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - check the code for Holberton School students.
+ *decription: use _putchar.c to print the alphabet
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char letra = 97;
+	int linea;
+
+	for (linea = 0; linea <= 9; linea++)
+	{
+		for (letra = 97; letra <= 122; letra++)
+		{
+			_putchar(letra);
+		}
+		
+		_putchar('\n');
+	}
+}
