4a48de771955785623773764a40e454f39bece05
Cmart94 <cristian.313@hotmail.com>
2021-02-09 20:44:14 +0000

    fix 1-alphabet.c message and return

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,19 @@
 #include "holberton.h"
 
 /**
- *main - check the code for Holberton School students.
- *decription: use _putchar.c
+ *print_alphabet - check the code for Holberton School students.
+ *decription: use _putchar.c to print the alphabet
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	char letra = 'a';
+
 	while (letra <= 'z')
 	{
 		_putchar(letra);
 		letra++;
 	}
 	_putchar('\n');
+	return (0);
 }
