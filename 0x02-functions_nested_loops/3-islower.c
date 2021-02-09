9ad18572e9e1d3edc1c85cc9488394511b5ea00a
Cmart94 <cristian.313@hotmail.com>
2021-02-09 22:50:31 +0000

    3-islower excersice

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <ctype.h>
+#include "holberton.h"
+
+/**
+ *_islower - funcion detectar minusculas
+ * @c: letras
+ * Description: use _putchar.c to print
+ * Return:  (Success)
+ */
+int _islower(int c)
+{
+	int resultLower = islower(c);
+
+	if (resultLower > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
