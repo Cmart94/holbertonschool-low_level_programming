920df4c6378d3ebaec4a9cc49e48d33f3cc07411
Cmart94 <cristian.313@hotmail.com>
2021-02-09 19:40:14 +0000

    fix holberton.h and 0-holberton.c files

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,5 +1,4 @@
 #include <holberton.h>
-#include <stdio.h>
 
 /**
  * main - Entry point
@@ -8,6 +7,15 @@
  */
 int main(void)
 {
-	_putchar("Holberton\n");
+	_putchar("H");
+	_putchar("o");
+	_putchar("l");
+	_putchar("b");
+	_putchar("e");
+	_putchar("r");
+	_putchar("t");
+	_putchar("o");
+	_putchar("n");
+	_putchar("\n");
 	return (0);
 }
