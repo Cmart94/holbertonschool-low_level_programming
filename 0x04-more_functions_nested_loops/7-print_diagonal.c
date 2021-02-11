8ce06ecc708e326a19184b13d0aa20f470bcd773
Cmart94 <cristian.313@hotmail.com>
2021-02-11 23:40:52 +0000

    fix 7-print.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - print diagonal in n times
+ *@n: number times
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				if (i == j)
+				{
+					_putchar('\\');
+					_putchar('\n');
+				}
+				else
+					_putchar(' ');
+			}
+		}
+	}
+}
