5a2c5054cf72c19f9d15e8cdf427473ba0daff8f
Cmart94 <cristian.313@hotmail.com>
2021-02-11 19:28:38 +0000

    2-mul.c excercise

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mult - function that multiplies two integers
+ * @a: integer a
+ * @b: integer b
+ * Return: multiplication of the integer
+ */
+int mul(int a, int b)
+{
+	int operation;
+
+	operation = a * b;
+
+	return (operation);
+}
