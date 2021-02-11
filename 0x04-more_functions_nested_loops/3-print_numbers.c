6b6fc31cef9a459393ec09e237016acafdb90d5a
Cmart94 <cristian.313@hotmail.com>
2021-02-11 19:43:24 +0000

    3-print_numbers.c excercise

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
