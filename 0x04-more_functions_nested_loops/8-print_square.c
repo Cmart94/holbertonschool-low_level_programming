eb6f4dec6023382d6f8d743c960aebab5536bf26
Cmart94 <cristian.313@hotmail.com>
2021-02-12 00:14:42 +0000

    8-print_square.c exercise

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_square - print a matrix n with #
+ *@n: number times
+ * Return: void
+ */
+void print_square(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
