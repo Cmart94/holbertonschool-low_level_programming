ac7c46555a8ea6317b101fbef889a37b56463285
Cmart94 <cristian.313@hotmail.com>
2021-02-16 20:33:18 +0000

    fix betty style 2-strlen.c and 3-puts.c files

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,9 +3,7 @@
 /**
  * _strlen - print the lenght of a string
  * @s: pointer of the string
- * @sizeString: final size
- * @n: array position
- * @c: char whith *s data.
+ *
  * Description : nothing to say
  * Return: lenght of the string without '\0'
  */
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,8 +3,6 @@
 /**
  * _puts - function that prints a string
  * @str: poiter of the string to print
- * @n: array index.
- * @c: char that stores *str
  *
  * Description: nothing to say
  * Return: void function
