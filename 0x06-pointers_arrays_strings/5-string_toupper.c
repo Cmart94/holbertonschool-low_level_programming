a511c36a411b451fd3889ea80967f99df4fdbb16
Cmart94 <cristian.313@hotmail.com>
2021-02-19 02:22:58 +0000

    exercise 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ * @s; string to modify
+ * Description: none
+ * Return: Char s modify
+ */
+char *string_toupper(char *s)
+{
+	int len = 0;
+
+	for ( ; s[len] != '\0'; len++)
+	{
+		if (s[len] >= 'a' && s[len] <= 'z')
+			s[len] = s[len] - 32;
+	}
+	return (s);
+}
