72c3ec85d85985268ec6c749a505577234c1e79c
Cmart94 <cristian.313@hotmail.com>
2021-02-22 21:53:16 +0000

    exersice 2-strchr.c file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: string where locate a character
+ * @c: characte to locate.
+ * Return: Pointer in the position if the character was locate
+ * or NULL if the character is no found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
