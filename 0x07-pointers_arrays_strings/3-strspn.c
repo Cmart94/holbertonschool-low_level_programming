641eeb6c30c3edce04b9fb28ef0607fca5d42175
Cmart94 <cristian.313@hotmail.com>
2021-02-23 04:27:13 +0000

    exersice 3-strspn.c exersice

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ *  _strspn - function that gets the length of a prefix substring
+ * @s: string to search within
+ * @accept: string that you want to find
+ * Return: number of bytes in the initial segment of s which consist
+ * only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, k = 0, m = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == a)
+		{
+			k++;
+			break;
+		}
+		else
+			k = 0;
+		if (k == 0)
+			break;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == '\0')
+			{
+				m++;
+				break;
+			}
+		}
+	}
+
+	return (m);
+}
