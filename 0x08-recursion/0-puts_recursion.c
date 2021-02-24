ee6c1dad119552b5588a1da9ef1a8b8bf5abb3dc
Cmart94 <cristian.313@hotmail.com>
2021-02-24 20:12:48 +0000

    Reademe and holberton.h files, exersice 0-puts_recursion file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - function that print a string with recursion
+ * @s: pointer to the string to print
+ * Return: Always 0 (Succes)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_putchar(*s);
+	_puts_recursion(++s);
+}
