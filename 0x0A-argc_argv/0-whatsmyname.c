0a2198b582c1fdfcb741d9011ebffc6159956169
Cmart94 <cristian.313@hotmail.com>
2021-03-02 02:25:37 +0000

    Fix atribute unused

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
  * @argv: array of strings with the arguments
  * Return: Number of arguments (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 
-	return (argc);
+	return (0);
 }
