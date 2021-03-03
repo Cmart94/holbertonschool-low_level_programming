f0b15c5b53962a2e7327ab24bb5d7de74aa40cee
Cmart94 <cristian.313@hotmail.com>
2021-03-03 22:29:19 +0000

    exersice 4 frees a 2 dimensional grid previously created by your alloc_grid function.

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid -  frees a 2 dimensional grid previously created
+ * @grid: 2 dimensional grid
+ * @height: rows of the grid
+ * Return: void function
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
