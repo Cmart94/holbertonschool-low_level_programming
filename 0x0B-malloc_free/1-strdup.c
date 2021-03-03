89be2412903722d039e6d57357992d8055bb7a66
Cmart94 <cristian.313@hotmail.com>
2021-03-03 03:19:18 +0000

    exersice 1-strdup.c script that copy a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copy of the string given as a parameter.
+ * @str: string for be copied
+ * Return: pointer to a copy string, or Null if str is null
+ */
+char *_strdup(char *str)
+{
+	int i, len = 0;
+	char *s;
+
+	while (str[len] != '\0')
+		len++;
+	s = malloc(sizeof(char) * len + 1);
+	if (s == NULL)
+		return (NULL);
+	if (s != NULL)
+	{
+		for (i = 0; i < len; i++)
+		{
+			s[i] = str[i];
+		}
+		s[len] = '\0';
+	}
+	return (s);
+}
