dba2ec1bc1e879f434f09549d66d0f4ef36b80f5
Cmart94 <cristian.313@hotmail.com>
2021-03-04 02:13:04 +0000

    fix null comparation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,14 +12,14 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0, len2 = 0, i, j = 0;
 	char *dest;
 
-	if (*s1 == '\0' || *s1 == NULL)
+	if (s1 == NULL || s1 == '\0')
 		len1 = 0;
 	else
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 			len1++;
 	}
-	if (*s2 == '\0' || *s2 == NULL)
+	if (s2 == NULL || s2 == '\0')
 		len2 = 0;
 	else
 	{
