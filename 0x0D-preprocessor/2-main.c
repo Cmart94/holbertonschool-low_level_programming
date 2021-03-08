3d494167c7b09b1083717b4effce5ef3b3a472f6
Cmart94 <cristian.313@hotmail.com>
2021-03-08 22:15:48 +0000

    print the name of the file that it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - programt that print the name of the file it was compiled frim.
+ * Return: 0 Succes
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
