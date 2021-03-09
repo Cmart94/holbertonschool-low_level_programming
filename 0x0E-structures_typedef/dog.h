ba31a3c1f3864bc5c07d45a410f4cdb62bcb71d2
Cmart94 <cristian.313@hotmail.com>
2021-03-09 00:00:18 +0000

    Readme.md file, exersice 00, dog info sheet

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - parameters info of the dog
+ * @name: pointer to the name
+ * @age: float for the age
+ * @owner: char to the name of the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
