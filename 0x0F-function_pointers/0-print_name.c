8c1b77c2aa12540f8bc1957a92ee79a6a9482d11
Cmart94 <cristian.313@hotmail.com>
2021-03-10 21:12:14 +0000

    Fix header and script condition

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,14 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - Function that call function to print the variable *name
  * @name: pointer to the name to print
- * @@f: ponter to the function
+ * @f: ponter to the function
  * Return: void function
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name != NULL && f != NULL)
 	f(name);
 }
