326ef3d623ed952e4dc86edafa8e8268ce00aec6
Cmart94 <cristian.313@hotmail.com>
2021-03-11 22:21:41 +0000

    01 exersice, print the numbers inside the arguments, variadic_functions.h update file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers in the arguments
+ * @separator: string ti be printed between numbers
+ * @n: number of integers passed to the function
+ * Return: void function.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+
+	if (n == 0)
+		exit(0);
+	va_start(arg, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%i", va_arg(arg, unsigned int));
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(arg);
+}
