f8d0baf1ed52aeaa188e4bd05471247679272148
Cmart94 <cristian.313@hotmail.com>
2021-03-24 00:18:41 +0000

    Recursive formto get the lenght of the list, and lists.h file update

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,13 +7,7 @@
  */
 size_t list_len(const list_t *h)
 {
-	int count = 0;
-	const list_t *tmp = h;
-
-	while (tmp != NULL)
-	{
-		count += 1;
-		tmp = tmp->next;
-	}
-	return (count);
+	if (h == NULL)
+		return (0);
+	return (1 + list_len(h->next));
 }
