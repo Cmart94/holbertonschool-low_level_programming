0c135939c4c53279386eee5ca56a5ec0ea42c45f
Cmart94 <cristian.313@hotmail.com>
2021-03-25 04:18:04 +0000

    betty style fixed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,8 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
-	for (i = 0; str[i] != '\0'; i++){}
+	for (i = 0; str[i] != '\0'; i++)
+	{}
 	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
