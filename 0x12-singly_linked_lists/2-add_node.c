1164f59e4aab220640f376b4be37990063f3196d
Cmart94 <cristian.313@hotmail.com>
2021-03-25 04:14:35 +0000

    Fix exersice 02, forbiden function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,12 +9,14 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	int i;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	for (i = 0; str[i] != '\0'; i++){}
+	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
 
