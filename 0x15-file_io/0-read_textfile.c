913e53f70450063d3e2815a12b2ae20d2721b942
Cmart94 <cristian.313@hotmail.com>
2021-03-29 23:32:26 +0000

    Fix NULL maloc exit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	if (buffer == NULL)
-		return (NULL);
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
