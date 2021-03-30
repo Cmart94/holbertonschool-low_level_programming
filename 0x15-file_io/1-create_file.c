9e6c18f752a4126690d327960dd982f19e77aa81
Cmart94 <cristian.313@hotmail.com>
2021-03-30 02:46:58 +0000

    Fix text_content exist Exersice 01

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
+	if (!text_content)
+		return (1);
 	for (len = 0; text_content[len] != '\0'; len++)
 	{}
 	r = write(fd, text_content, len);
