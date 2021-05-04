196c87b2631a029865af2dfaf830f450234b7f66
Cmart94 <cristian.313@hotmail.com>
2021-05-04 02:57:47 +0000

    exersice 01, make a dynamic library

---
+++ 0x18-dynamic_libraries/holberton.h
@@ -0,0 +1,29 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void _islower(void);
+void _isalpha();
+void _abs();
+void _isupper(void);
+void _isdigit(void);
+int _strlen(char *s);
+void _puts(void);
+void _strcpy(void);
+void _atoi(void);
+void _strcat(void);
+void _strncat(void);
+void _strncpy(void);
+void _strcmp(void);
+void _memset(void);
+void _memcpy(void);
+void _strchr(void);
+void _strspn(void);
+void _strpbrk(void);
+void _strstr(void);
+
+#endif /* HOLBERTON_H_ */
