df341a56bcf3ad25b985d1dc53c100f718309eb5
Cmart94 <cristian.313@hotmail.com>
2021-05-29 23:48:21 +0000

    create hash taable fixing script

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,9 +8,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = malloc(size * (sizeof(hash_table_t)));
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 
 	if (hash_table == NULL)
+	{
+		free(hash_table);
 		return (NULL);
+	}
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t));
+	if (hash_table->array == NULL)
+	{
+		free(hash_table->array);
+		free(hash_table);
+		return (NULL);
+	}
 	return (hash_table);
 }
