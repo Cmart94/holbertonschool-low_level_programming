280878620034819142b2ba34c39632dd48141f2b
Cmart94 <cristian.313@hotmail.com>
2021-05-30 01:50:51 +0000

    03 exersice

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+	unsigned long int index;
+  
+	if (node == NULL)
+		return (0);
+	if (key == NULL)
+		return (0);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] == NULL)
+	{
+		node->next = NULL;
+		ht->array[index] = node;
+	}
+	else
+	{
+		node->next = ht->array[index];
+		ht->array[index] = node;
+	}
+	return (1);
+}
